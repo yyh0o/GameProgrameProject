0bd4bd65c226bd747c1014c8866cada186bbaadb
yyh0o <646167686@qq.com>
2020-04-27 22:01:05 +0800

    双缓存解决频闪问题

---
+++ Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yyh0o on 2020/4/27.
+//
+
+#include "Renderer.h"
---
+++ Renderer.h
@@ -0,0 +1,16 @@
+//
+// Created by yyh0o on 2020/4/27.
+//
+
+#ifndef MINI_GAME_RENDERER_H
+#define MINI_GAME_RENDERER_H
+
+
+
+class Renderer {
+
+};
+
+
+
+#endif //MINI_GAME_RENDERER_H
